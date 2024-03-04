bd27a6c37295c2c5900415da7c51f70180be8f29
Farhanuddin Ahmed <90610771+farhan4995@users.noreply.github.com>
2024-03-04 11:43:10 +0530

    Update AnimateGraph.m

--- AnimateGraph.m
+++ AnimateGraph.m
@@ -1,7 +1,7 @@
 A = importdata('ForceCoefficient.dat');
-x = A(:,1);
-y = A(:,4);
-z = A(:,5);
+x = A(:,1);    % 1st column as x axis
+y = A(:,4);    % 4th column as y axis
+z = A(:,5);    % 5th column as z axis
 % Initialize video
 myVideo = VideoWriter('Lift_110_50'); %open video file
 myVideo.Quality=100;
@@ -9,39 +9,24 @@ myVideo.FrameRate = 10;  %can adjust this, 5 - 10 works well for me
 open(myVideo)
 
 figure(1);
-% subplot(2,1,2);
-% plot(x,z,'r','LineWidth',1)
-% xlabel('Cycle Time','FontSize',12)
-% ylabel('C_{D} per wing','FontSize',12)
-% axis([0 1 -8 30]);
-% hold on
-% p = plot(x(1),z(1),'ro','MarkerFaceColor','r',...
-%                    'YDataSource','Z',...
-%                    'XDataSource','X');
-% 
-% subplot(2,1,1);
-plot(x,y,'k','LineWidth',1)
-xlabel('Cycle Time','FontSize',12)
-ylabel('Coefficient of Lift, C_{L}','FontSize',12)
-axis([0 1 -4 7]);
+plot(x,y,'k','LineWidth',1)    % Plotting y with respect to x axis
+xlabel('Cycle Time','FontSize',12)  %Label x axis
+ylabel('Coefficient of Lift, C_{L}','FontSize',12) %Label y axis and choose font size
+axis([0 1 -4 7]);  % Choose x-limit and y-limit of graph
 hold on
 h = plot(x(1),y(1),'ko','MarkerFaceColor','k',...
                    'YDataSource','Y',...
-                   'XDataSource','X');
-% h = subplot(x(1),z(1),'ro','MarkerFaceColor','r',...
-%                    'YDataSource','Z',...
-%                    'XDataSource','X');
-for t = 1:100:numel(x)
+                   'XDataSource','X');    % Placing a pointer at 1st value
+
+for t = 1:100:numel(x)      %Create a for loop with spacing of 100th value of data
     X = x(t);
     Y = y(t);
     Z = z(t);
-    refreshdata(h,'caller');
-%     refreshdata(p,'caller');
-    %drawnow;
+    refreshdata(h,'caller');    % Refresh the data pointer at 100th interval
     
     set(gcf,'color','w');
     frame = getframe(gcf); %get frame
-    writeVideo(myVideo, frame);
+    writeVideo(myVideo, frame);      % Write the video
     pause(.5);
 end
 close(myVideo)
